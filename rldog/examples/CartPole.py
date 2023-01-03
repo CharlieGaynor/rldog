a968b38a282717726f64714daaf9cd9747630ab0
Charlie <CharlieJackGaynor@Gmail.com>
2023-01-03 18:09:57 +0000

    Cleaning files

--- rldog/examples/CartPole.py
+++ rldog/examples/CartPole.py
@@ -6,6 +6,7 @@ import logging
 from rldog.agents.policy_gradients.PPO import PPO
 from rldog.configs.CartPole_config import CartPoleConfig
 from rldog.tools.logger import logger
+import os
 
 if __name__ == "__main__":
     logger.setLevel(logging.INFO)
@@ -13,5 +14,6 @@ if __name__ == "__main__":
     conf = CartPoleConfig()
     conf.PPO_config()
     agent = PPO(conf)
-    # agent.play_games(plot=True, save_model_at_checkpoints=True, model_directory='rldog/saved_models/CartPole_PPO_4_2_2_64_03012023')
+    # model_dir = os.path.join(os.getcwd(), 'saved_models/CartPole_PPO_4_2_2_64_03012023')
+    # agent.play_games(plot=True, save_model_at_checkpoints=True, model_directory=model_dir)
     # agent.evaluate_games(100, plot=True, log_things=True)
